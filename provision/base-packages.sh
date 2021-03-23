73d72893465a3419eb5dd1acb94ac4ce60428e0e
Bruno <55158870+bruno-canada@users.noreply.github.com>
2021-03-22 20:46:53 -0700

    Minor comment adjustment to base-package.sh

--- provision/base-packages.sh
+++ provision/base-packages.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 
-# Uncomment the following commands if your are not using Vagrant box "bruno-canada/alpine312-docker"
-# The commands below were created for base Alpine Linux
-
-# # Change DNS records to Cloudflare
+# Change DNS records to Cloudflare
 echo "nameserver 1.1.1.1" > /etc/resolv.conf
 echo "nameserver 1.0.0.1" >> /etc/resolv.conf
 
-# Install base packages
+############################################################
+# Uncomment the following commands if your are not using Vagrant box "bruno-canada/alpine312-docker"
+# The commands below were created for base Alpine Linux
+
+# # Install base packages
 # apk update
 
 # # Install docker
