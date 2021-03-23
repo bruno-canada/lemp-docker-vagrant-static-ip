438e5dfd5135781163b966ff506557f5c4e0751d
Bruno <55158870+bruno-canada@users.noreply.github.com>
2021-03-22 20:34:53 -0700

    Added custom Vagrant Box with Docker and Docker Compose

--- provision/base-packages.sh
+++ provision/base-packages.sh
@@ -1,21 +1,24 @@
 #!/usr/bin/env bash
 
-# Change DNS records to Cloudflare
+# Uncomment the following commands if your are not using Vagrant box "bruno-canada/alpine312-docker"
+# The commands below were created for base Alpine Linux
+
+# # Change DNS records to Cloudflare
 echo "nameserver 1.1.1.1" > /etc/resolv.conf
 echo "nameserver 1.0.0.1" >> /etc/resolv.conf
 
 # Install base packages
 # apk update
 
-# Install docker
-apk add docker-compose docker
+# # Install docker
+# apk add docker-compose docker
 
-# Handle groups and permissions
-adduser vagrant docker
+# # Handle groups and permissions
+# adduser vagrant docker
 
-# Add docker and docker-compose to boot
-rc-update add docker
+# # Add docker and docker-compose to boot
+# rc-update add docker
 
-# Reboot services
-rc-service docker restart
+# # Reboot services
+# rc-service docker restart
 
